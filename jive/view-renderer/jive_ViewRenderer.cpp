6fbba22e50e1efc2c53a86e9d9d861046a358937
James Johnson <james.johnson@focusrite.com>
2021-10-11 20:56:50 +0100

    Add case for juce::ToggleButton to jive::ViewRenderer

--- jive/view-renderer/jive_ViewRenderer.cpp
+++ jive/view-renderer/jive_ViewRenderer.cpp
@@ -4,9 +4,17 @@
 namespace jive
 {
     //==================================================================================================================
+    bool treeHasMatchingTypeIgnoringCase (const juce::ValueTree& tree, const juce::String& expectedType)
+    {
+        return tree.getType().toString().equalsIgnoreCase(expectedType);
+    }
+
+    //==================================================================================================================
     std::unique_ptr<juce::Component> ViewRenderer::createView(juce::ValueTree sourceTree) const
     {
-        juce::ignoreUnused(sourceTree);
+        if (treeHasMatchingTypeIgnoringCase(sourceTree, "ToggleButton"))
+            return std::make_unique<juce::ToggleButton>();
+
         return nullptr;
     }
 } // namespace jive
--- test-runner/source/view-renderer/test_ViewRenderer.cpp
+++ test-runner/source/view-renderer/test_ViewRenderer.cpp
@@ -14,18 +14,30 @@ public:
     void runTest() final
     {
         testEmptyTree();
+        testTreeWithToggleButtonType();
     }
 
 private:
     //==================================================================================================================
     void testEmptyTree()
     {
-        beginTest("Rendering a view with an empty value tree should return a nullptr");
+        beginTest("Rendering a view from an empty value tree should return a nullptr");
 
         jive::ViewRenderer renderer;
 
         const auto view = renderer.createView({});
         expect(view == nullptr);
+    }
+
+    void testTreeWithToggleButtonType()
+    {
+        beginTest("Rendering a view from a value tree with a type of 'ToggleButton' should return a "
+                  "juce::ToggleButton");
+
+        jive::ViewRenderer renderer;
+
+        const auto view = renderer.createView(juce::ValueTree{ "ToggleButton" });
+        expect(dynamic_cast<juce::ToggleButton*>(view.get()) != nullptr);
     }
 };
 
