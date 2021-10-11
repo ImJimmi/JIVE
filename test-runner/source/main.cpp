141f05d7aa7b03c69f2d7cf9b3b6b647bcadbb7c
James Johnson <james.johnson@focusrite.com>
2021-10-11 19:17:35 +0100

    Initial project setup

---
+++ test-runner/source/main.cpp
@@ -0,0 +1,57 @@
+#include <juce_gui_basics/juce_gui_basics.h>
+
+//======================================================================================================================
+auto getNumFailures(const juce::UnitTestRunner& runner)
+{
+    auto numFails = 0;
+
+    for (auto i = 0; i < runner.getNumResults(); i++)
+    {
+        if (auto* result = runner.getResult(i))
+            numFails += result->failures;
+    }
+
+    return numFails;
+    ;
+}
+
+//======================================================================================================================
+class TestRunner : public juce::JUCEApplication
+{
+public:
+    //==================================================================================================================
+    TestRunner() = default;
+
+    //==================================================================================================================
+    const juce::String getApplicationName() final
+    {
+        return "JIVE Test Runner";
+    }
+
+    const juce::String getApplicationVersion() final
+    {
+        return "1.0.0";
+    }
+
+    void initialise(const juce::String&) final
+    {
+        juce::UnitTestRunner runner;
+        runner.setAssertOnFailure(false);
+
+        runner.runTestsInCategory("JIVE");
+
+        setApplicationReturnValue(getNumFailures(runner));
+        quit();
+    }
+
+    void shutdown() final
+    {
+    }
+
+private:
+    //==================================================================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(TestRunner)
+};
+
+//======================================================================================================================
+START_JUCE_APPLICATION(TestRunner)
