55827498b83de9781db0769a484c5916ffe4b573
James Johnson <james.johnson@focusrite.com>
2021-10-11 20:12:53 +0100

    Add jive::ViewRenderer::renderView

--- jive/view-renderer/jive_ViewRenderer.cpp
+++ jive/view-renderer/jive_ViewRenderer.cpp
@@ -3,4 +3,10 @@
 //======================================================================================================================
 namespace jive
 {
+    //==================================================================================================================
+    std::unique_ptr<juce::Component> ViewRenderer::createView(juce::ValueTree sourceTree) const
+    {
+        juce::ignoreUnused(sourceTree);
+        return nullptr;
+    }
 } // namespace jive
