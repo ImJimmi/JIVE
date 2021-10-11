9d67f91463a9b9c5a5a9ce7823f41b1eebd65498
James Johnson <james.johnson@focusrite.com>
2021-10-11 21:11:00 +0100

    Implement Component IDs in jive::ViewRenderer

--- test-runner/source/view-renderer/test_ViewRenderer.cpp
+++ test-runner/source/view-renderer/test_ViewRenderer.cpp
@@ -16,6 +16,7 @@ public:
         testEmptyTree();
         testTreeWithToggleButtonType();
         testTreeWithTextButtonType();
+        testTreeWithID();
     }
 
 private:
@@ -50,6 +51,65 @@ private:
 
         const auto view = renderer.createView(juce::ValueTree{ "TextButton" });
         expect(dynamic_cast<juce::TextButton*>(view.get()) != nullptr);
+    }
+
+    void testTreeWithID()
+    {
+        jive::ViewRenderer renderer;
+
+        {
+            beginTest("Rendering a view from a value tree with a type of 'ToggleButton' and a blank 'id' property "
+                      "should return a component with no ID");
+
+            juce::ValueTree tree{ "ToggleButton" };
+
+            const auto view = renderer.createView(tree);
+            expectEquals(view->getComponentID(), juce::String{});
+        }
+
+        {
+            beginTest("Rendering a view from a value tree with a type of 'ToggleButton' and an 'id' property with the "
+                      "value '123' should return a component with the ID '123'");
+
+            juce::ValueTree tree{ "ToggleButton" };
+            tree.setProperty("id", "123", nullptr);
+
+            const auto view = renderer.createView(tree);
+            expectEquals(view->getComponentID(), juce::String{ "123" });
+        }
+
+        {
+            beginTest("Rendering a view from a value tree with a type of 'ToggleButton' and an 'id' property with the "
+                      "value '567' should return a component with the ID '567'");
+
+            juce::ValueTree tree{ "ToggleButton" };
+            tree.setProperty("id", "567", nullptr);
+
+            const auto view = renderer.createView(tree);
+            expectEquals(view->getComponentID(), juce::String{ "567" });
+        }
+
+        {
+            beginTest("Rendering a view from a value tree with a type of 'TextButton' and an 'id' property with the "
+                      "value '987' should return a component with the ID '987'");
+
+            juce::ValueTree tree{ "TextButton" };
+            tree.setProperty("id", "987", nullptr);
+
+            const auto view = renderer.createView(tree);
+            expectEquals(view->getComponentID(), juce::String{ "987" });
+        }
+
+        {
+            beginTest("Rendering a view from a value tree with a type of 'TextButton' and an 'id' property with the "
+                      "value '543' should return a component with the ID '543'");
+
+            juce::ValueTree tree{ "TextButton" };
+            tree.setProperty("id", "543", nullptr);
+
+            const auto view = renderer.createView(tree);
+            expectEquals(view->getComponentID(), juce::String{ "543" });
+        }
     }
 };
 
