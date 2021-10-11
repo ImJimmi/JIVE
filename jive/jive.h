690aa942ab84b28dd39f04bf59b24305fd0837ea
James Johnson <james.johnson@focusrite.com>
2021-10-11 20:12:05 +0100

    Add jive::ViewRenderer

---
+++ jive/jive.h
@@ -0,0 +1,17 @@
+#pragma once
+
+/* BEGIN_JUCE_MODULE_DECLARATION
+    ID:                 jive
+    vendor:             James Johnson
+    version:            0.1.0
+    name:               James' Intuitive View Explicator
+    license:            GPL/Commercial
+    minimumCppStandard: 14
+    dependencies:       juce_gui_basics
+END_JUCE_MODULE_DECLARATION */
+
+//======================================================================================================================
+#include <juce_gui_basics/juce_gui_basics.h>
+
+//======================================================================================================================
+#include "view-renderer/jive_ViewRenderer.h"
