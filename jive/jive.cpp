0501a4fdcfb1e071d6118a237de990d50b73801a
James Johnson <james.johnson@focusrite.com>
2021-11-28 11:14:11 +0000

    Move flex classes to dedicated folder

--- jive/jive.cpp
+++ jive/jive.cpp
@@ -4,8 +4,8 @@
 #include "core/gui-items/jive_GuiItem.cpp"
 #include "core/gui-items/jive_GuiItemDecorator.cpp"
 
-#include "core/gui-items/jive_GuiFlexContainer.cpp"
-#include "core/gui-items/jive_GuiFlexItem.cpp"
+#include "core/gui-items/flex/jive_GuiFlexContainer.cpp"
+#include "core/gui-items/flex/jive_GuiFlexItem.cpp"
 #include "core/gui-items/widgets/jive_ComboBox.cpp"
 #include "core/gui-items/widgets/jive_Label.cpp"
 
