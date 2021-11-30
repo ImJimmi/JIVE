b7a864c1d830edbc6a9ddce6bd5dd1c7eacef809
James Johnson <james.johnson@focusrite.com>
2021-11-30 16:09:34 +0000

    Only allow one test-runner instance at a time

--- test-runner/source/main.cpp
+++ test-runner/source/main.cpp
@@ -40,6 +40,11 @@ public:
     {
     }
 
+    bool moreThanOneInstanceAllowed() final
+    {
+        return false;
+    }
+
 private:
     //==================================================================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(TestRunner)
