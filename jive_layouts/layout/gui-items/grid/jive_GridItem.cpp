8bb82bf772de455c8b1182cbd6b024f7efd9779f
James Johnson <james.johnson@focusrite.com>
2022-03-29 22:41:48 +0100

    Add juce::GridItem conversion to jive::GridItem

--- jive_layouts/layout/gui-items/grid/jive_GridItem.cpp
+++ jive_layouts/layout/gui-items/grid/jive_GridItem.cpp
@@ -8,6 +8,14 @@ namespace jive
         : GuiItemDecorator{ std::move(itemToDecorate) }
     {
     }
+
+    //==================================================================================================================
+    GridItem::operator juce::GridItem()
+    {
+        juce::GridItem gridItem{ getComponent() };
+
+        return gridItem;
+    }
 } // namespace jive
 
 //======================================================================================================================
@@ -22,9 +30,26 @@ public:
 
     void runTest() final
     {
+        testComponent();
     }
 
 private:
+    std::unique_ptr<jive::GridItem> createGridItem(juce::ValueTree tree)
+    {
+        return std::make_unique<jive::GridItem>(std::make_unique<jive::GuiItem>(std::make_unique<juce::Component>(),
+                                                                                tree));
+    }
+
+    void testComponent()
+    {
+        beginTest("component");
+
+        juce::ValueTree tree{ "Component" };
+        auto item = createGridItem(tree);
+
+        auto gridItem = static_cast<juce::GridItem>(*item);
+        expect(gridItem.associatedComponent == &item->getComponent());
+    }
 };
 
 static GridItemUnitTest gridItemUnitTest;
