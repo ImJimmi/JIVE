b73ec46227e1d26b15bb7d199081686fdbc2f0b4
James Johnson <james.johnson@focusrite.com>
2022-05-08 18:57:09 +0100

    Add x and y properties to block-item

--- jive_layouts/layout/gui-items/block/jive_BlockItem.cpp
+++ jive_layouts/layout/gui-items/block/jive_BlockItem.cpp
@@ -6,7 +6,20 @@ namespace jive
     //==================================================================================================================
     BlockItem::BlockItem(std::unique_ptr<GuiItem> itemToDecorate)
         : GuiItemDecorator{ std::move(itemToDecorate) }
+        , x{ tree, "x" }
+        , y{ tree, "y" }
     {
+        x.onValueChange = [this]() {
+            const auto nearestX = juce::roundToInt(x);
+            auto& component = getComponent();
+            component.setTopLeftPosition(component.getPosition().withX(nearestX));
+        };
+        y.onValueChange = [this]() {
+            const auto nearestY = juce::roundToInt(y);
+            auto& component = getComponent();
+            component.setTopLeftPosition(component.getPosition().withY(nearestY));
+        };
+        getComponent().setTopLeftPosition({ juce::roundToInt(x), juce::roundToInt(y) });
     }
 
     //==================================================================================================================
@@ -24,9 +37,46 @@ public:
 
     void runTest() override
     {
+        testPosition();
     }
 
 private:
+    std::unique_ptr<jive::BlockItem> createBlockItem(juce::ValueTree tree)
+    {
+        return std::make_unique<jive::BlockItem>(std::make_unique<jive::GuiItem>(std::make_unique<juce::Component>(),
+                                                                                 tree));
+    }
+
+    void testPosition()
+    {
+        beginTest("position");
+
+        {
+            juce::ValueTree tree{ "Component" };
+            const auto item = createBlockItem(tree);
+
+            expect(item->getComponent().getX() == 0);
+            expect(item->getComponent().getY() == 0);
+
+            tree.setProperty("x", 10.4f, nullptr);
+            tree.setProperty("y", 20.89f, nullptr);
+            expect(item->getComponent().getX() == 10);
+            expect(item->getComponent().getY() == 21);
+        }
+        {
+            juce::ValueTree tree{
+                "Component",
+                {
+                    { "x", 15 },
+                    { "y", 25 },
+                },
+            };
+            const auto item = createBlockItem(tree);
+
+            expect(item->getComponent().getX() == 15);
+            expect(item->getComponent().getY() == 25);
+        }
+    }
 };
 
 static BlockItemTest blockItemTest;
