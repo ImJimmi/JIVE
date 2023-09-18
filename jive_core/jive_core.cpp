3038b21774d2bc7807496c354449d77be0c31d6f
James Johnson <ImJimmi@protonmail.com>
2023-09-18 23:10:21 +0100

    Add to/from `juce::var` conversion functions

--- jive_core/jive_core.cpp
+++ jive_core/jive_core.cpp
@@ -14,6 +14,7 @@
 #include "values/variant-converters/jive_FlexVariantConverters.cpp"
 #include "values/variant-converters/jive_GridVariantConverters.cpp"
 #include "values/variant-converters/jive_MiscVariantConverters.cpp"
+#include "values/variant-converters/jive_VariantConvertion.cpp"
 
 #include "geometry/jive_BoxModel.cpp"
 #include "geometry/jive_Length.cpp"
