5067ce09dbcf7237bd3a6b08dfe529a85a7b2b55
James J <ImJimmi@protonmail.com>
2024-06-06 21:21:38 +0100

    Add `melatonin_inspector` to demo runner

--- runners/demo-runner/source/jive_demo/Application.h
+++ runners/demo-runner/source/jive_demo/Application.h
@@ -4,6 +4,7 @@
 
 #include <jive_demo/gui/WindowPresenter.h>
 #include <jive_demo/gui/tokens/Typography.h>
+#include <melatonin_inspector/melatonin_inspector.h>
 
 namespace jive_demo
 {
@@ -26,11 +27,15 @@ namespace jive_demo
         {
             window = interpreter.interpret(windowPresenter.present());
             interpreter.listenTo(*window);
+
+            inspector = std::make_unique<melatonin::Inspector>(*window->getComponent(), false);
+            inspector->setVisible(true);
         }
 
         void shutdown() final
         {
             window = nullptr;
+            inspector = nullptr;
         }
 
     private:
@@ -39,6 +44,8 @@ namespace jive_demo
         jive::Interpreter interpreter;
         WindowPresenter windowPresenter{ state.getWindowState(), "Window" };
         std::unique_ptr<jive::GuiItem> window;
+
+        std::unique_ptr<melatonin::Inspector> inspector;
 
         JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Application)
     };
