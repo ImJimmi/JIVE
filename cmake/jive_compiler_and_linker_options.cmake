2a1790ec3e999085f0d4393d81df8c9486f26851
James J <ImJimmi@protonmail.com>
2024-08-01 16:50:48 +0100

    Add `.cmake-format`

--- cmake/jive_compiler_and_linker_options.cmake
+++ cmake/jive_compiler_and_linker_options.cmake
@@ -1,45 +1,46 @@
 include_guard()
 
-add_library(jive_compiler_and_linker_options INTERFACE)
-add_library(jive::compiler_and_linker_options ALIAS jive_compiler_and_linker_options)
+add_library(jive_compiler_and_linker_options
+            INTERFACE
+)
+add_library(jive::compiler_and_linker_options
+            ALIAS
+            jive_compiler_and_linker_options
+)
 
 target_compile_definitions(jive_compiler_and_linker_options
-INTERFACE
-    JUCE_DISABLE_JUCE_VERSION_PRINTING=1
-    JUCE_SILENCE_XCODE_15_LINKER_WARNING=1
-    JUCE_USE_CURL=0
-    JUCE_WEB_BROWSER=0
+                           INTERFACE JUCE_DISABLE_JUCE_VERSION_PRINTING=1
+                                     JUCE_SILENCE_XCODE_15_LINKER_WARNING=1
+                                     JUCE_USE_CURL=0
+                                     JUCE_WEB_BROWSER=0
 )
 
 if (APPLE)
     target_compile_options(jive_compiler_and_linker_options
-    INTERFACE
-        -Werror
+                           INTERFACE -Werror
     )
 
     target_link_options(jive_compiler_and_linker_options
-    INTERFACE
-        -Wl,-weak_reference_mismatches,weak
+                        INTERFACE
+                        -Wl,-weak_reference_mismatches,weak
     )
 
     if (JIVE_ENABLE_SANITISERS)
         target_compile_options(jive_compiler_and_linker_options
-        INTERFACE
-            -fsanitize=address
-            -fsanitize=undefined
+                               INTERFACE -fsanitize=address
+                                         -fsanitize=undefined
         )
 
         target_link_options(jive_compiler_and_linker_options
-        INTERFACE
-            -fsanitize=address
-            -fsanitize=undefined
+                            INTERFACE
+                            -fsanitize=address
+                            -fsanitize=undefined
         )
-    endif()
-endif()
+    endif ()
+endif ()
 
 if (MSVC)
     target_compile_options(jive_compiler_and_linker_options
-    INTERFACE
-        /WX
+                           INTERFACE /WX
     )
-endif()
+endif ()
