593c1d5c1948c59027e0dfc4a8b6971187686a6c
James J <ImJimmi@protonmail.com>
2024-08-01 16:51:34 +0100

    Add `package-lock.cmake`

---
+++ package-lock.cmake
@@ -0,0 +1,6 @@
+CPMDeclarePackage(JUCE
+                  GIT_TAG 8.0.1
+                  GITHUB_REPOSITORY juce-framework/JUCE
+                  SYSTEM YES
+                  EXCLUDE_FROM_ALL YES
+)
