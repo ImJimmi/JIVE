045af6a43afb14f3cf741745a1a05a685bb6d762
James J <ImJimmi@protonmail.com>
2025-02-27 20:34:41 +0000

    Use `develop` branch of JUCE

--- package-lock.cmake
+++ package-lock.cmake
@@ -1,5 +1,5 @@
 CPMDeclarePackage(JUCE
-                  GIT_TAG 8.0.6
+                  GIT_TAG develop
                   GITHUB_REPOSITORY juce-framework/JUCE
                   SYSTEM YES
                   EXCLUDE_FROM_ALL YES
