f6c356a2e73661ddf47dbd2464113e06f5ffd2c9
James J <ImJimmi@protonmail.com>
2025-11-07 00:19:02 +0000

    Use latest JUCE

--- package-lock.cmake
+++ package-lock.cmake
@@ -1,5 +1,5 @@
 CPMDeclarePackage(JUCE
-                  GIT_TAG develop
+                  GIT_TAG 8.0.9
                   GITHUB_REPOSITORY juce-framework/JUCE
                   SYSTEM YES
                   EXCLUDE_FROM_ALL YES
